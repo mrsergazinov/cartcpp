2cf211f6ddb426a7db1cd47e07d6ac4ead1800c2
Renat Sergazinov <mrsergazinov@tamu.edu>
2020-11-17 17:11:12 -0600

    Changed package strcuture to accomodate R Modules

---
+++ R/Tree-exports.R
@@ -0,0 +1,1 @@
+loadModule(module = "RcppTreeEx", TRUE)
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,55 +6,11 @@
 
 using namespace Rcpp;
 
-// rcpparma_hello_world
-arma::mat rcpparma_hello_world();
-RcppExport SEXP _cartcpp_rcpparma_hello_world() {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(rcpparma_hello_world());
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_outerproduct
-arma::mat rcpparma_outerproduct(const arma::colvec& x);
-RcppExport SEXP _cartcpp_rcpparma_outerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_outerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_innerproduct
-double rcpparma_innerproduct(const arma::colvec& x);
-RcppExport SEXP _cartcpp_rcpparma_innerproduct(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_innerproduct(x));
-    return rcpp_result_gen;
-END_RCPP
-}
-// rcpparma_bothproducts
-Rcpp::List rcpparma_bothproducts(const arma::colvec& x);
-RcppExport SEXP _cartcpp_rcpparma_bothproducts(SEXP xSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::colvec& >::type x(xSEXP);
-    rcpp_result_gen = Rcpp::wrap(rcpparma_bothproducts(x));
-    return rcpp_result_gen;
-END_RCPP
-}
+
+RcppExport SEXP _rcpp_module_boot_RcppTreeEx();
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_cartcpp_rcpparma_hello_world", (DL_FUNC) &_cartcpp_rcpparma_hello_world, 0},
-    {"_cartcpp_rcpparma_outerproduct", (DL_FUNC) &_cartcpp_rcpparma_outerproduct, 1},
-    {"_cartcpp_rcpparma_innerproduct", (DL_FUNC) &_cartcpp_rcpparma_innerproduct, 1},
-    {"_cartcpp_rcpparma_bothproducts", (DL_FUNC) &_cartcpp_rcpparma_bothproducts, 1},
+    {"_rcpp_module_boot_RcppTreeEx", (DL_FUNC) &_rcpp_module_boot_RcppTreeEx, 0},
     {NULL, NULL, 0}
 };
 
