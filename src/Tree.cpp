d1b58bc3b7c2792df05681d529646f44ad9334db
Renat Sergazinov <mrsergazinov@tamu.edu>
2020-12-03 13:29:52 -0600

    Added constructors for classes

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -2,10 +2,20 @@
 // Retrieve the definition of our Tree class
 #include "Tree.h"
 
-// Constructors ----
+// Constructors
+Node::Node(const int& d): _depth(d) {}
+
 Tree::Tree() {}
 
+Tree::Tree(const int& ident, const int& treeType, const arma::uword& numFeatures,
+           const int& maxDepth, const int& minCount):
+  _id(ident),
+  _treeType(treeType),
+  _numFeatures(numFeatures),
+  _maxDepth(maxDepth),
+  _minCount(minCount) {}
 
+// Methods
 arma::mat Tree::add(const arma::mat& A, const arma::mat& B) {
   return (A + B);
 }
