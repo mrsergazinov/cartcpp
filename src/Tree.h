06a6059efefab816571f76700a6cbfe635876416
Renat Sergazinov <mrsergazinov@tamu.edu>
2020-12-03 13:29:38 -0600

    Changed defintions of Tree and Node classes: for node made all the methods and fields public; for tree changed some of the data types to the appropariate arma.

--- src/Tree.h
+++ src/Tree.h
@@ -4,47 +4,52 @@
 #include "RcppArmadillo.h"
 // [[Rcpp::depends(RcppArmadillo)]]
 
-
 class Node {
 public:
+  // methods
   Node();
   Node(const int& d);
   Node(const Node& other);
   // ~Node();
 
-protected:
+public:
+  // fields
   Node* _left;
   Node* _right;
   int _depth;
+  arma::mat* _dataSet;
+  arma::colvec* _label;
   int _featureIndex;
   double _splitValue;
   double _classResult;
-  arma::mat* _dataSet;
 };
 
 class Tree {
 public:
+  // constructors
   Tree();
-  Tree(const int& id, const int& treeType, const double& testRatio,
+  Tree(const int& ident, const int& treeType, const arma::uword& numFeatures,
        const int& maxDepth, const int& minCount);
 
-  void train(const arma::mat& X);
+  // public methods
+  void train(arma::mat& X, arma::colvec& Y);
   arma::colvec predict(const arma::mat& X) const;
-  arma::mat add(const arma::mat& A, const arma::mat& B);
 
 protected:
+  // protected methods
   void buildTree(Node* nd);
   bool stop(const Node* nd) const;
   bool split(Node* nd);
-  void classResult(Node* nd);
-  double gini(const arma::mat& X, const arma::uword& featureId, const double& featureVal) const;
-  double mse(const arma::mat& X, const arma::uword& featureId, const double& featureVal) const;
+  void classResult(Node* nd, const int treeType);
+  double gini(const arma::mat& X, const arma::colvec& Y, const arma::uword& featureId, const double& featureVal) const;
+  double mse(const arma::mat& X, const arma::colvec& Y, const arma::uword& featureId, const double& featureVal) const;
 
 protected:
-  int _id;
+  // protected fields
+  arma::uword _numFeatures; // ratio of features selected at each split
   Node* _root;
-  int _treeType;   // treeType 0: classification tree OR 1: regression tree
-  double _testRatio; // ratio of features selected at each split
+  int _id;
+  int _treeType; // treeType 0: classification tree OR 1: regression tree
   int _maxDepth; // maxDepth of tree
   int _minCount; // min count of points for a leaf
 };
