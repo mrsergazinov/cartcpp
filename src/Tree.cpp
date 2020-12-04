a7884260991bedc6ba204e758e7236c9cb1e8996
Renat Sergazinov <mrsergazinov@tamu.edu>
2020-12-03 21:16:25 -0600

    Added new skeletons for Tree methods: stop, classResult, split

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -25,18 +25,58 @@ void Tree::train(arma::mat &X, arma::colvec &Y) {
   //TODO: make compatibility checks
 
   _root = new Node(0); // create root node
-  _root->_dataSet = &X; // feed data to the root node
-  _root->_label = &Y;
-  Tree::buildTree(_root); // start building the tree
+  _root->_beginRow = 0; // feed data to the root node
+  _root->_endRow = X.n_rows - 1;
+  Tree::buildTree(_root, X, Y); // start building the tree
 }
 
-void Tree::buildTree(Node* node){
+void Tree::buildTree(Node* nd, arma::mat &X, arma::colvec &Y){
+  // Input: node, data
+  // Output: none
+  // Process: check if the node can be split, split the node, continue building the tree
 
-  if (Tree::stop(node)){
-    classResult(node, _treeType);
+  if (Tree::stop(nd, X, Y)){
+    classResult(nd, X, Y);
   } else{
-    split(node);
-    Tree::buildTree(node->_left);
-    Tree::buildTree(node->_right);
+    split(nd, X, Y);
+    Tree::buildTree(nd->_left, X, Y);
+    Tree::buildTree(nd->_right, X, Y);
   }
+}
+
+void Tree::split(Node *nd, arma::mat &X, arma::colvec &Y) {
+  // Input: node, data
+  // Output: none
+  // Process: split the node
+  arma::colvec subsetIndex = arma::shuffle(arma::linspace(0, X.n_cols - 1)); //shuffle linear space of column indices
+  subsetIndex = subsetIndex.subvec(0, _numFeatures - 1); // select the first numFeatures from the shuffled linear space
+
+  for (arma::uword feature : subsetIndex) {
+
+  }
+
+  nd->_left = new Node(nd->_depth + 1);
+  nd->_left = new Node(nd->_depth + 1);
+
+}
+
+bool Tree::stop(const Node* nd, arma::mat &X, arma::colvec &Y) const {
+  // Input: node, data
+  // Output: boolean indicating whether the node can be split
+  // Process: check if the node can be split
+
+  // TODO: fill-in the code
+  // check that depth is less than maxDepth
+  // check that more than minCount number of points for a leaf
+  // check that the label pool is not homogenuous for a classification tree
+  return false;
+}
+
+void Tree::classResult(Node* nd, arma::mat &X, arma::colvec &Y) const {
+  // Input: node, data
+  // Output: none
+  // Process: calculate leaf value based on the data
+
+  //TODO: fill-in the code
+  nd->_classResult = 1;
 }
